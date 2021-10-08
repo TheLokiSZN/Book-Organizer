7cf2b2ddb4eca0b286385c292cfa943c2327e2c6
Juanita\Lukas Roberson <lukeroberson24@yahoo.com>
2021-10-08 18:56:09 -0400

    partial working code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,42 @@ const resolvers = {
     Query: {
         me: async(parent, args, context) => {
             if (context.user) {
-                const userData
+                const userData = await User.findOne({_id: context.user._id}).select('-__v -password')
+                return userData
+            }
+            throw new AuthenticationError('You are not logged in')
+        }
+    },
+    Mutation: {
+        addUser: async(parent, args) => {
+            const user = await User.create(args)
+            const token = signToken(user)
+            return { token, user }
+        },
+        login: async(parent, { email, password }) => {
+            const user = await user.findOne({email})
+            if (!user) {
+                throw new AuthenticationError('Incorrect user')
+            }
+            const correctPassword = await user.isCorrectPassword(password)
+            if (!correctPassword) {
+                throw new AuthenticationError('Incorrect password')
+            }
+            const token = signToken(user)
+            return { token, user }
+        },
+        saveBook: async(parent, { bookData }, context) => {
+            if (context.user) {
+                const updatedUser = await user.findByIdAndUpdate(
+                    {_id: context.user._id}
+                    {saveBook:}
+                    {}
+                )
+            }
+        },
+        removeBook : async(parent, {bookData}, context) => {
+            if (context.user) {
+                const 
             }
         }
     }
